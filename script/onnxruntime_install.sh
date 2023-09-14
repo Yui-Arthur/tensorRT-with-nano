a3fc622b21e41848a06c770a1ad2db249da990d7
Yui-Arthur <arthur80633@gmail.com>
2023-09-15 00:20:35 +0800

    fix onnxruntime install sh bug

--- script/onnxruntime_install.sh
+++ script/onnxruntime_install.sh
@@ -1,3 +1,2 @@
-wget -O onnxruntime_gpu-1.11.0-cp36-cp36m-linux_aarch64.whl
- https://nvidia.box.com/shared/static/pmsqsiaw4pg9qrbeckcbymho6c01jj4z.whl
-pip3 install onnxruntime_gpu-1.11.0-cp36-cp36m-linux_aarch64.whl
+wget -O onnxruntime_gpu-1.11.0-cp36-cp36m-linux_aarch64.whl https://nvidia.box.com/shared/static/pmsqsiaw4pg9qrbeckcbymho6c01jj4z.whl
+sudo  pip3 install onnxruntime_gpu-1.11.0-cp36-cp36m-linux_aarch64.whl
